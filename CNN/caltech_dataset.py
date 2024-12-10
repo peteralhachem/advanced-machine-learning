5a27ee3b2526f8369fecce3eed5bf3b337a601ae
Peter <s293885@studenti.polito.it>
2024-12-10 09:57:26 +0200

    Modify CNN

--- CNN/caltech_dataset.py
+++ CNN/caltech_dataset.py
@@ -15,66 +15,88 @@ def pil_loader(path):
         return img.convert('RGB')
 
 
+IMG_EXTENSIONS = ('.jpg', '.jpeg', '.png', '.ppm', '.bmp', '.pgm', '.tif', '.tiff', '.webp')
+
+def make_dataset(dir, class_to_idx, extensions=None, is_valid_file=None, split='train', transform='None') : 
+    # dir = 'Caltech101'
+    images = []
+    if not ((extensions is None) ^ (is_valid_file is None)):
+        raise ValueError("Both extensions and is_valid_file cannot be None or not None at the same time")
+    if extensions is not None:
+        def is_valid_file(x):
+            return x.lower().endswith(extensions)
+
+    inputFile = os.path.join(dir, split + '.txt') # 'Caltech101/{split}.txt'
+    with open(inputFile, 'r') as f:      
+      input_images = f.read().splitlines()
+
+    root = os.path.join(dir, '101_ObjectCategories/') # 'Caltech101/101_ObjectCategories/'
+
+    for fname in input_images:
+      fpath = os.path.split(fname)
+      # print(fpath) # 'accordion' 'image_0002.jpg'
+      target = fpath[0] # 'accordion'
+      path = os.path.join(root, fname) # 'Caltech101/101_ObjectCategories/accordion/image_0002.jpg'
+      if is_valid_file(path) and target != 'BACKGROUND_Google':
+        item = (path, class_to_idx[target])
+        images.append(item)
+
+    return images # paths
+
 class Caltech(VisionDataset):
+    ''' Caltech 101 Dataset '''
+
     def __init__(self, root, split='train', transform=None, target_transform=None):
+        '''
+          Args:
+            root (string): Directory with all the images.
+            split (string): Defines the split you are going to use (split files are called 'train.txt' and 'test.txt').
+            transform (callable, optional): Optional transform to be applied on a sample.
+            target_transform (callable, optional): Optional trasform to be applied on the target labels (not used).
+        '''
         super(Caltech, self).__init__(root, transform=transform, target_transform=target_transform)
+        self.split = split
+        
+        classes, class_to_idx = self._find_classes(self.root)
+        samples = make_dataset(self.root, class_to_idx, IMG_EXTENSIONS, split=self.split, transform=transform)
+        if len(samples) == 0:
+            raise (RuntimeError("Found 0 files in subfolders of: " + self.root + "\n"
+                                "Supported extensions are: " + ",".join(extensions)))
 
-        self.split = split  # This defines the split you are going to use
-        # (split files are called 'train.txt' and 'test.txt')
-        self.transform = transform
-        self.target_transform=target_transform
-        self.dataset = []
-        self.classes = []
+        self.loader = pil_loader
+        self.classes = classes
+        self.class_to_idx = class_to_idx
+        self.samples = samples
+        self.targets = [s[1] for s in samples]
 
-
-        '''
-        - Here you should implement the logic for reading the splits files and accessing elements
-        - If the RAM size allows it, it is faster to store all data in memory
-        - PyTorch Dataset classes use indexes to read elements
-        - You should provide a way for the __getitem__ method to access the image-label pair
-          through the index
-        - Labels should start from 0, so for Caltech you will have labels 0...100 (excluding the background class) 
-        '''
-        with open('./{}.txt'.format(self.split), 'r') as data_file:
-            for line in data_file.readlines():
-                path = './' + os.path.join(root, line)
-                path = path.replace('\\', '/')
-                path = path.replace('\n', '')
-                image_class = path.split('/')[2]
-                if image_class == 'BACKGROUND_Google':
-                    continue
-                img = pil_loader(path)
-                if image_class not in self.classes:
-                    self.classes.append(image_class)
-
-                self.dataset.append((img, self.classes.index(image_class)))
-
-
+    def _find_classes(self, dir):        
+        root = os.path.join(dir, '101_ObjectCategories/')
+        classes = [d.name for d in os.scandir(root) if d.is_dir()]
+        classes.remove('BACKGROUND_Google')
+        classes.sort()                
+        #print(classes)
+        class_to_idx = {classes[i]: i for i in range(len(classes))}
+        #print(class_to_idx)
+        return classes, class_to_idx
 
     def __getitem__(self, index):
-        """
+        '''
         __getitem__ should access an element through its index
         Args:
             index (int): Index
-
         Returns:
             tuple: (sample, target) where target is class_index of the target class.
-        """
+        '''
+        path, label = self.samples[index]
+        sample = self.loader(path)
 
-        image, label = self.dataset[index]  # Provide a way to access image and label via index
-        # Image should be a PIL Image
-        # label can be int
-
-        # Applies preprocessing when accessing the image
         if self.transform is not None:
-            image = self.transform(image)
+            image = self.transform(sample)
 
         return image, label
 
-    def __len__(self):
-        """
+    def __len__(self):   
+        '''
         The __len__ method returns the length of the dataset
-        It is mandatory, as this is used by several other components
-        """
-        length = len(self.dataset)  # Provide a way to get the length (number of elements) of the dataset
-        return length
+        '''     
+        return len(self.samples)
